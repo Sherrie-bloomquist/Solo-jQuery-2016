e3fb0e11b91a3eeff157f8207845055ca0f970f2
Sherrie Bloomquist <sherrie.bloomquist@yahoo.com>
2016-12-07 08:14:47 -0600

    try again

--- script.js
+++ script.js
@@ -5,5 +5,34 @@ var blue = 0;
 
 $(document).ready(function(){
 
-  
+  $('.color-button').on('click', function(){
+    var selectedColor = $(this).data('color');
+    console.log(selectedColor);
+
+
+    if(selectedColor == 'red'){
+    red++;
+      $('#red').html('Total red: ' + red);
+      $(".container").append("<div class='color-cube red'></div>");
+
+    } else if(selectedColor == 'yellow'){
+      yellow++;
+        $('#yellow').html('Total yellow: ' + yellow);
+        $(".container").append("<div class='color-cube yellow'></div>");
+
+    } else if(selectedColor == 'green'){
+    green++;
+    $('#green').html('Total green: ' + green);
+    $(".container").append("<div class='color-cube green'></div>");
+
+  } else if(selectedColor == 'blue') {
+      blue++;
+      $('#blue').html('Total blue: ' + blue);
+      $(".container").append("<div class='color-cube blue'></div>");
+
+
+
+   }
+});
+
 });//end doc ready
